20c880e03eac4861cb2e2fece8e7a412f5994427
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-05-31 23:14:56 +0530

    Create

---
+++ TrendsAnalyser/src/main/java/com/trendsAnalyser/app/TrendsAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.trendsAnalyser.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendsAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendsAnalyserApplication.class, args);
+	}
+
+}
---
+++ TrendsAnalyser/src/test/java/com/trendsAnalyser/app/TrendsAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trendsAnalyser.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendsAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
