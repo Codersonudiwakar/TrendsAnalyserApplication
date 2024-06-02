23d504606b2f93a3b10093784967732f0066267b
Codersonudiwakar <138717372+Codersonudiwakar@users.noreply.github.com>
2024-06-02 23:23:26 +0530

    commit

--- TrendsAnalyser/src/main/java/com/trendsAnalyzer/app/TrendsAnalyserApplication.java
+++ TrendsAnalyser/src/main/java/com/trendsAnalyzer/app/TrendsAnalyserApplication.java
@@ -1,4 +1,4 @@
-package com.trendsAnalyser.app;
+package com.trendsAnalyzer.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
